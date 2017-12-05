f4d7d12a167d4633de2725574ae9dc5375c616c9
Mher <poghosyanusc@gmail.com>
2017-12-04 22:22:16 -0800

    Added skeleton test class for Ocean and instruction/comments for Main method in BattleShipGame class

---
+++ src/test/java/OceanTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OceanTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void placeAllShipsRandomly() throws Exception {
+    }
+
+    @Test
+    public void isOccupied() throws Exception {
+    }
+
+    @Test
+    public void shootAt() throws Exception {
+    }
+
+    @Test
+    public void getShotsFired() throws Exception {
+    }
+
+    @Test
+    public void getHitCount() throws Exception {
+    }
+
+    @Test
+    public void getShipsSunk() throws Exception {
+    }
+
+    @Test
+    public void isGameOver() throws Exception {
+    }
+
+    @Test
+    public void getShipArray() throws Exception {
+    }
+
+    @Test
+    public void print() throws Exception {
+    }
+
+}
