bda71e9c93e6a51674ae82c81b11ad7ccad67e8d
Mher <poghosyanusc@gmail.com>
2017-12-11 19:09:32 -0800

    Cleaned up BattleShipGame and added TODOs

--- src/main/java/BattleShipGame.java
+++ src/main/java/BattleShipGame.java
@@ -5,19 +5,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class BattleShipGame {
-    /*
-        The BattleshipGame class is the ”main” class–that is, it contains a main method.
-        In this class you will set up the game;
-        accept ”shots” from the user;
-        display the results; print final scores;
-        and ask the user if he/she wants to play again.
-
-        All input/output is done here (although some of it is done by calling a print() method
-        in the Ocean class.) All computation will be done in the Ocean class and the various Ship classes. Note that you
-        want to accept 5 shots from the user. So you need to ensure that you have a well defined format for this.
-        For example you can provide an instruction to the user as follows:
-        The input format should look like this: 1, 1; 0, 3; 7, 3; 9, 11; 12, 17
-    */
+    //TODO Can we make this prettier?
     public static void main(String[] args) {
         Ocean ocean = new Ocean();
         Scanner in = new Scanner(System.in);
@@ -30,7 +18,6 @@ public class BattleShipGame {
 
             while (!isGameOver(ocean)) {
                 inputArray = getInput(in);
-
                 for (String s : inputArray) {
                     hitShip = ocean.shootAt(Integer.parseInt(s.substring(0, 1)), Integer.parseInt(s.substring(3)));
                     displayHitResult(hitShip);
@@ -40,10 +27,8 @@ public class BattleShipGame {
 
             System.out.println("The game is over!");
             System.out.println("There were " + ocean.getShotsFired() + " shots fired");
-
             System.out.println("Would you like to play again? (Y/N)");
             String restart = in.next();
-
             if (restart.equals("Y") || restart.equals("y")) {
                 ocean = new Ocean();
                 break start;
@@ -65,6 +50,7 @@ public class BattleShipGame {
         ocean.placeAllShipsRandomly();
     }
 
+    //TODO Throw exception when user input doesn't match correct input, how should that case be handled?
     private static List<String> getInput(Scanner in) {
         System.out.println("Please input the 5 positions you would like to shop at.");
         System.out.println("Input format should look like so: 1, 1; 0, 3; 7, 3; 9, 11; 12, 17");
